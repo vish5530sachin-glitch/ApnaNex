301b384a636687ef5be049808cc9c76ad5d3af76
freeebatch <koushalanurag0@gmail.com>
2025-07-10 11:58:21 +0530

    Add files via upload

--- Extractor/html_converter/bot.py
+++ Extractor/html_converter/bot.py
@@ -794,20 +794,20 @@ def generate_html(file_name, videos, pdfs, others):
     </div>
     <div class="container-fluid py-5 text-center">
         <h1 class="brand-title mb-4">
-<a href="https://t.me/GodxBots" style="text-decoration: none; color: inherit;">
-  <i class="fas fa-bolt"></i> GOD x UG <i class="fas fa-bolt"></i>
+<a href="https://t.me/batchxfree" style="text-decoration: none; color: inherit;">
+  <i class="fas fa-bolt"></i> BATCH x ARK <i class="fas fa-bolt"></i>
 </a>
 
                 </h1>
         <h2 class="header-title mb-4">{file_name_without_extension}</h2>
         <p class="lead">
-<a href="https://t.me/GodxBots" style="text-decoration: none; color: inherit;">
+<a href="https://t.me/batchxfree" style="text-decoration: none; color: inherit;">
   <span class="badge bg-primary me-2">
-    <i class="fas fa-robot"></i> Join @GodxBots
+    <i class="fas fa-robot"></i> Join @batchxfree
   </span>
 </a>
 
-            <a href="http://t.me/GodxBOts" class="text-decoration-none">
+            <a href="http://t.me/batchxfree" class="text-decoration-none">
                 <span class="badge bg-warning">
                     <i class="fas fa-bolt"></i>UG 
                 </span>
@@ -875,7 +875,7 @@ def generate_html(file_name, videos, pdfs, others):
         f'<div class="item-actions">'
         f'<button class="btn-action view" onclick="viewPDF(\'{obfuscate_url(url)}\')" title="View PDF">'
         f'<i class="fas fa-eye"></i></button>'
-        f'<button class="btn-action download" onclick="downloadFile(\'{obfuscate_url(url)}\', \'{name} - @GodxBots.pdf\')" title="Download PDF">'
+        f'<button class="btn-action download" onclick="downloadFile(\'{obfuscate_url(url)}\', \'{name} - @batchxfree.pdf\')" title="Download PDF">'
         f'<i class="fas fa-download"></i></button>'
         f'</div>'
         f'</div>'
@@ -1216,9 +1216,9 @@ async def handle_txt2html(client: Client, message: Message):
         # Generate HTML
         html_content = generate_html(file_name, videos, pdfs, others)
         
-        # Save HTML file with @GodxBots suffix
+        # Save HTML file with @batchxfree suffix
         base_name = os.path.splitext(file_name)[0]
-        html_file_name = f"{base_name}_@GodxBots.html"
+        html_file_name = f"{base_name}_@batchxfree.html"
         html_file_path = os.path.join(os.path.dirname(file_path), html_file_name)
         
         with open(html_file_path, "w", encoding='utf-8') as f:
